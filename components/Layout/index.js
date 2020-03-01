3fa964d1db6fe9581f1710e1b9fddb991816e178
Jakob Heuser <jakob@felocity.com>
2020-02-29 23:21:29 -0800

    ✨ Feat: breakpoint based submenu work started

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,9 +1,13 @@
 import { Link } from "../Typography";
+import { useBreakpoint } from "~/hooks/useBreakpoints";
+import { useWindowScroll } from "react-use";
 import Box from "../Box";
 import Head from "next/head";
 import Logo from "../Logo";
 import NextLink from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+// import { useTheme} from "emotion-theming"
 
 const NavLink = ({ href, children, ...rest }) => (
   <NextLink href={href} passHref>
@@ -11,7 +15,36 @@ const NavLink = ({ href, children, ...rest }) => (
   </NextLink>
 );
 
+const MENU_HEIGHT = "3rem";
+
+const MenuDrop = styled(Box)`
+  box-shadow: rgba(67, 90, 111, 0.3) 0px 0px 1px,
+    rgba(67, 90, 111, 0.47) 0px 2px 4px -2px;
+  opacity: 0;
+  ${p => (p.show ? p.theme.animations.fadeIn : p.theme.animations.fadeOut)}
+`;
+
+const links = [
+  { href: "/thoughts", label: "Writing" },
+  { href: "/oss", label: "Open Source" },
+  { href: "/about", label: "About" },
+];
+
+const SlideNav = ({ ...all }) => {
+  return <Box {...all}>A</Box>;
+};
+
 const Layout = ({ children = [], title = null }) => {
+  const { y } = useWindowScroll();
+  const bp = useBreakpoint();
+  // eslint-disable-next-line no-unused-vars
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (!bp.md) return;
+    setSubmenuOpen(false); // reset submenu
+  }, [bp]);
+
   return (
     <>
       <Head>
@@ -27,11 +60,34 @@ const Layout = ({ children = [], title = null }) => {
         background="olive"
         marginLeft="auto"
         marginRight="auto"
-        padding="2"
+        paddingBottom="2"
       >
-        <Box display="flex" flexDirection="row">
+        <MenuDrop
+          position="fixed"
+          height={MENU_HEIGHT}
+          left="0px"
+          top="0px"
+          width="100vw"
+          maxWidth="100%"
+          show={y > 0}
+          aria-hidden
+        />
+        <Box
+          display="flex"
+          flexDirection="row"
+          position="fixed"
+          height={MENU_HEIGHT}
+          background="red"
+          width="100%"
+          maxWidth="48rem"
+        >
           <NextLink href="/" passHref>
-            <Logo as="a" title="Home" maxWidth="10rem" />
+            <Logo
+              as="a"
+              title="Home"
+              maxWidth="10rem"
+              maxHeight={MENU_HEIGHT}
+            />
           </NextLink>
           <Box
             flexGrow="1"
@@ -40,23 +96,29 @@ const Layout = ({ children = [], title = null }) => {
             flexDirection="row"
             alignItems="center"
             marginLeft="2"
+            marginRight="2"
+            justifyContent={bp.xs ? "end" : "start"}
           >
-            <NavLink href="/thoughts" marginLeft="1">
-              Writing
-            </NavLink>
-            <NavLink href="/oss" marginLeft="1">
-              Open Source
-            </NavLink>
-            <NavLink marginLeft="1" href="/about">
-              About
-            </NavLink>
+            {bp.sm &&
+              links.map(l => (
+                <NavLink key={l.label} href={l.href} marginLeft="1">
+                  {l.label}
+                </NavLink>
+              ))}
+            {bp.xs && <SlideNav marginLeft="1" />}
           </Box>
         </Box>
-        <Box>{children}</Box>
+        <Box
+          paddingTop={MENU_HEIGHT}
+          paddingLeft="2"
+          paddingRight="2"
+          minHeight="200vh"
+        >
+          <Box paddingTop="2">{children}</Box>
+        </Box>
       </Box>
     </>
   );
 };
-Layout.displayName = "Layout";
 
 export default Layout;
--- style/theme.js
+++ style/theme.js
@@ -1,3 +1,43 @@
+import { css, keyframes } from "@emotion/core";
+
+const ANIMATION_TIME = 0.42;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0
+  }
+  to {
+    opacity: 1
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity:1
+  }
+  to {
+    opacity: 0
+  }
+`;
+
+const enterSlideLeft = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+
+const exitSlideRight = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(100%);
+  }
+`;
+
 const theme = {
   fontSizes: [8, 12, 16, 24, 32, 48, 64, 96, 128, 256],
   space: [
@@ -36,6 +76,30 @@ const theme = {
     white: "#fff",
     lightWash: "#e2e2e2",
     darkWash: "#dadada",
+  },
+  animations: {
+    fadeIn: css`
+      opacity: 0;
+      will-change: opacity;
+      animation: ${fadeIn} ${ANIMATION_TIME}s ease-out forwards;
+    `,
+    fadeOut: css`
+      opacity: 1;
+      will-change: opacity;
+      animation: ${fadeOut} ${ANIMATION_TIME}s ease-out forwards;
+    `,
+    enterSlideLeft: css`
+      transform: translateX(100%);
+      will-change: transform;
+      animation: ${enterSlideLeft} ${ANIMATION_TIME}s ease-out forwards,
+        ${fadeIn} ${ANIMATION_TIME}s ease-out forwards;
+    `,
+    exitSlideRight: css`
+      transform: translateX(0);
+      will-change: transform;
+      animation: ${exitSlideRight} ${ANIMATION_TIME}s ease-out forwards,
+        ${fadeOut} ${ANIMATION_TIME}s ease-out forwards;
+    `,
   },
 };
 
