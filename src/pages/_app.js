fd678688a04b1fec4cdb62a4dbc763acf7a700b1
Jakob Heuser <jakob@felocity.com>
2021-03-30 01:30:08 -0700

    feat: Adds great fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,11 @@ const App = ({ Component, pageProps }) => {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cloud.typography.com/7828318/7442832/css/fonts.css"
+        />
       </Head>
       <Providers>
         <Component {...pageProps} />
