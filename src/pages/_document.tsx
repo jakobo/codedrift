0db321a194bfd624bf4fa28e7565f10f3bb11f7b
Jakob Heuser <jakob@codedrift.com>
2022-05-26 18:53:05 -0700

    feat: Adds favicon stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,27 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&family=Roboto+Mono:wght@300&family=Work+Sans:wght@300&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#047857" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#f5f5f4" />
         </Head>
         <body
           className="bg-gray-100 bg-paper-stone-200 dark:bg-gray-900 dark:bg-paper-stone-800 text-gray-800 dark:text-gray-100"
