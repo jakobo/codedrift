0c2ce1619ad051aaedb6b38a8d35d9db197f106f
Jakob Heuser <jakob@codedrift.com>
2022-10-30 16:59:31 -0700

    feat: Added previous designs to the colophon

--- pages/colophon.tsx
+++ pages/colophon.tsx
@@ -1,11 +1,50 @@
 import { Layout } from "components/Layout";
-import React from "react";
+import React, { useState } from "react";
 import { PROSE, SECTION_HEADLINE } from "data/constants";
 import Link from "next/link";
 import cx from "classnames";
 import { NextSeo } from "next-seo";
+import Image, { StaticImageData } from "next/image";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+import i20010502 from "../public/images/legacy/20010502.png";
+import i20020206 from "../public/images/legacy/20020206.png";
+import i20020320 from "../public/images/legacy/20020320.png";
+import i20020604 from "../public/images/legacy/20020604.png";
+import i20030210 from "../public/images/legacy/20030210.png";
+import i20031022 from "../public/images/legacy/20031022.png";
+import i20040831 from "../public/images/legacy/20040831.png";
+import i20050329 from "../public/images/legacy/20050329.png";
+import i20060411 from "../public/images/legacy/20060411.png";
+import i20061012 from "../public/images/legacy/20061012.png";
+import i20100102 from "../public/images/legacy/20100102.png";
+import i20140814 from "../public/images/legacy/20140814.png";
+import i20161026 from "../public/images/legacy/20161026.png";
+import i20200224 from "../public/images/legacy/20200224.png";
+import i20220525 from "../public/images/legacy/20220525.png";
+
+const previousDesigns = [
+  i20010502,
+  i20020206,
+  i20020320,
+  i20020604,
+  i20030210,
+  i20031022,
+  i20040831,
+  i20050329,
+  i20060411,
+  i20061012,
+  i20100102,
+  i20140814,
+  i20161026,
+  i20200224,
+  i20220525,
+].reverse();
 
 const Colophon: React.FC<{}> = () => {
+  const [index, setIndex] = useState(-1);
+
   return (
     <>
       <NextSeo
@@ -78,6 +117,65 @@ const Colophon: React.FC<{}> = () => {
           </p>
           <p>We are only limited by the limits we place.</p>
         </div>
+        <h2 className="text-2xl pt-8 pb-4">Previous Versions</h2>
+        <section>
+          <div className="flex flex-row flex-wrap gap-6">
+            {previousDesigns.map((m, i) => (
+              <div
+                key={m.src}
+                className="relative w-[100px] h-[100px] border-2 border-gray-400"
+              >
+                <Image
+                  src={m}
+                  layout="fill"
+                  placeholder="blur"
+                  objectFit="cover"
+                  width="100"
+                  height="100"
+                  alt=""
+                  onClick={() => setIndex(i)}
+                  className="cursor-pointer"
+                />
+              </div>
+            ))}
+          </div>
+          <Lightbox
+            open={index >= 0}
+            index={index}
+            close={() => setIndex(-1)}
+            slides={previousDesigns}
+            render={{
+              slide: (image, offset, rect) => {
+                return (
+                  <div
+                    style={{
+                      position: "relative",
+                      width: image.width,
+                      height: image.height,
+                      maxWidth: "80vw",
+                    }}
+                  >
+                    <Image
+                      src={image as StaticImageData}
+                      layout="fill"
+                      loading="eager"
+                      placeholder="blur"
+                      objectFit="contain"
+                      alt={"alt" in image ? image.alt : ""}
+                      sizes={
+                        typeof window !== "undefined"
+                          ? `${Math.ceil(
+                              (image.width / window.innerWidth) * 100
+                            )}vw`
+                          : `${image.width}px`
+                      }
+                    />
+                  </div>
+                );
+              },
+            }}
+          />
+        </section>
       </Layout>
     </>
   );
