bf3aa566bce5d3d1c6ecee295352f3ba78f95063
Jakob Heuser <jakob@codedrift.com>
2023-12-08 18:42:19 -0800

    feat: Adds icons for all socials

--- pages/social.tsx
+++ pages/social.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { NextSeo } from "next-seo";
 import Link from "next/link.js";
-import { Layout } from "@/components/Layout.js";
+import {
+  ArrowCircleRightIcon,
+  CloudIcon,
+} from "@heroicons/react/solid/index.js";
+import { GitHubIcon } from "@/components/icons/Github.js";
+import { MastodonIcon } from "@/components/icons/Mastodon.js";
+import { InstagramIcon } from "@/components/icons/Instagram.js";
+import { LinkedInIcon } from "@/components/icons/LinkedIn.js";
+import { ThreadsIcon } from "@/components/icons/Threads.js";
 import { PROSE, SECTION_HEADLINE } from "@/data/constants.js";
+import { Layout } from "@/components/Layout.js";
+
+const ICON_STYLE = "inline-block h-4 w-4 fill-primary-400 mr-2 -mt-1";
 
 const Colophon: React.FC = () => {
   return (
@@ -27,9 +38,10 @@ const Colophon: React.FC = () => {
             <br />
             <span className="text-xl">🫰🏻❤️</span>
           </p>
-          <ul>
+          <ul className="list-none">
             <li>
               <p>
+                <ThreadsIcon className={ICON_STYLE} />
                 Posting on Threads & the fediverse as{" "}
                 <Link rel="me" href="https://www.threads.net/@codedrift.social">
                   @codedrift.social@threads.net
@@ -38,6 +50,7 @@ const Colophon: React.FC = () => {
             </li>
             <li>
               <p>
+                <InstagramIcon className={ICON_STYLE} />
                 Sharing photos on Instagram as{" "}
                 <Link
                   rel="me"
@@ -49,6 +62,7 @@ const Colophon: React.FC = () => {
             </li>
             <li>
               <p>
+                <CloudIcon className={ICON_STYLE} />
                 Shitposting on Bluesky with{" "}
                 <Link href="https://bsky.app/profile/codedrift.com">
                   @codedrift.com
@@ -57,6 +71,7 @@ const Colophon: React.FC = () => {
             </li>
             <li>
               <p>
+                <GitHubIcon className={ICON_STYLE} />
                 Coding open source on GitHub under the username{" "}
                 <Link rel="me" href="https://github.com/jakobo/">
                   jakobo
@@ -65,6 +80,7 @@ const Colophon: React.FC = () => {
             </li>
             <li>
               <p>
+                <LinkedInIcon className={ICON_STYLE} />
                 Maintaining a professional profile on LinkedIn as{" "}
                 <Link rel="me" href="https://www.linkedin.com/in/jakobheuser/">
                   Jakob Heuser
@@ -73,6 +89,7 @@ const Colophon: React.FC = () => {
             </li>
             <li>
               <p>
+                <MastodonIcon className={ICON_STYLE} />
                 Keeping a backup Fediverse account on a Mastodon server, just in
                 case Meta never makes good on that ActivityPub promise:{" "}
                 <Link rel="me" href="https://hachyderm.io/@jakobo">
@@ -82,6 +99,7 @@ const Colophon: React.FC = () => {
             </li>
             <li>
               <p>
+                <ArrowCircleRightIcon className={ICON_STYLE} />
                 Squatting on my X/Twitter handle, @jakobo, with no intention to
                 return
               </p>
